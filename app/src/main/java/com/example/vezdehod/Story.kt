953415e9253daf5894c9ef8317f27d67a07ed6b8
LAPTOP-465MA33N\Армен <rediraph@mail.ru>
2022-06-04 13:13:53 +0700

    story dataset

---
+++ app/src/main/java/com/example/vezdehod/Story.kt
@@ -0,0 +1,10 @@
+package com.example.vezdehod
+
+data class Story(
+    val city: String,
+    val date: String,
+    val title: String,
+    val description: String,
+    val link: String,
+    val imageUrl: String
+)

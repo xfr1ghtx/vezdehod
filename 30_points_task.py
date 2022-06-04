9aa8de722c0e274f8ac84be6c0e596aba475d51c
Eduard Khusnutdinov <ed.khusnutdinov@gmail.com>
2022-06-04 17:40:12 +0700

    Обновил подсчет id

--- 30_points_task.py
+++ 30_points_task.py
@@ -4,19 +4,25 @@ import json
 def count_tries(users):
       link = "https://codeforces.com/api/user.status?handle="
       for user in users:
-          request_info = requests.get(link+user)
-          json_obj = dict(request_info.json())
+        request_info = requests.get(link+user)
+        json_obj = dict(request_info.json())
 
-          contests=[]
+        contests=[]
+        
+        for submissions in json_obj['result']:
+            prob = submissions.get('problem')
+            if prob.get('contestId'):
+                contests.append(
+                    str(prob.get('contestId'))+prob.get('index')
+                )
+            else:
+                contests.append(
+                    prob.get('index')
+                )
 
-          for submissions in json_obj['result']:
-              contests.append(
-                  submissions.get('problem').get('contestId')
-              )
-
-          print(
-              user,len(set(contests))
-          )
+        print(
+            user,len(set(contests))
+        )
 
 
 students = input().split() # Список логинов через пробел. Например: login1 login2 login3

53241f8cfe8273b57e72a95efb338df3ba00f922
VladimirMynka <vladimirmynka@yandex.ru>
2022-06-04 18:06:28 +0700

    Отформатировал код

--- 30_points_task.py
+++ 30_points_task.py
@@ -1,19 +1,20 @@
 import requests
 import json
 
+
 def count_tries(users):
-      link = "https://codeforces.com/api/user.status?handle="
-      for user in users:
-        request_info = requests.get(link+user)
+    link = "https://codeforces.com/api/user.status?handle="
+    for user in users:
+        request_info = requests.get(link + user)
         json_obj = dict(request_info.json())
 
-        contests=[]
-        
+        contests = []
+
         for submissions in json_obj['result']:
             prob = submissions.get('problem')
             if prob.get('contestId'):
                 contests.append(
-                    str(prob.get('contestId'))+prob.get('index')
+                    str(prob.get('contestId')) + prob.get('index')
                 )
             else:
                 contests.append(
@@ -21,9 +22,9 @@ def count_tries(users):
                 )
 
         print(
-            user,len(set(contests))
+            user, len(set(contests))
         )
 
 
-students = input().split() # Список логинов через пробел. Например: login1 login2 login3
+students = input().split()  # Список логинов через пробел. Например: login1 login2 login3
 count_tries(students)

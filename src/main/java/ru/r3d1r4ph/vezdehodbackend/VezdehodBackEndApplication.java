cd03e454be27a568eb2b8602bf119aad289967ef
LAPTOP-465MA33N\Армен <rediraph@mail.ru>
2022-06-05 13:47:02 +0700

    project loaded

---
+++ src/main/java/ru/r3d1r4ph/vezdehodbackend/VezdehodBackEndApplication.java
@@ -0,0 +1,13 @@
+package ru.r3d1r4ph.vezdehodbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VezdehodBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VezdehodBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/r3d1r4ph/vezdehodbackend/VezdehodBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.r3d1r4ph.vezdehodbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VezdehodBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

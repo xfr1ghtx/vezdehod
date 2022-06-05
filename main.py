24c22e4d3a5b72afbcd0d4765c903a0e47bc5332
VladimirMynka <vladimirmynka@yandex.ru>
2022-06-05 11:38:18 +0700

    Marusya 3

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import json
 
 def create_question(text, answers, category):
     return {
-        text: text,
-        answers: answers
+        'text': text,
+        'answers': answers
     }
 
 
@@ -29,14 +29,24 @@ def create_response(session, response, **kwargs):
 users = {}
 
 questions = [
-    create_question("^Что^ вывед`ет пр`ограмма?\nlet a = 5\nlet b = 7\nconsole.log({a+b}{ а плюс ^бэ^ })", ("12"), 'algorithms'),
-    create_question("На каком языке пишут приложения для IOS?", ("swift", "свифт", "обжектив-си", "objective-c"), 'mobile'),
-    create_question("Как называется ^самый^ распространённый язык для работы с реляционными базами данных?", ("sql"), 'backend'),
-    create_question("Как называется вид инъекции, направленный на взлом реляционных баз данных?", ("sql", "sql-injection", "sql-инъекция"), 'security'),
-    create_question("Интерфейс для взаимодействия других программ с вашим сервисом", ["api", "апи"], 'backend'),
-    create_question("Заполните многоточие, чтобы выдать всем пользователям все права для этого файла: \nsudo chmod " + "<...> /vezdekod/vezdehits.txt?", ["777"], 'security'),
-    create_question("Какую сред`у разработки используют для создания android-приложений?", ["android studio, androidstudio"], 'mobile'),
-    create_question("Как`ой `алгоритм имеет в ^худшем^ случае асимпт`отитическую сложность {nlogn}{эн лог эн}?" + "\n1. quickSort(X)\n2. bubbleSort(X)\n3. mergeSort(X)", ["3"], 'algorithms')
+    create_question("^Что^ вывед`ет пр`ограмма?\nlet a = 5\nlet b = 7\nconsole.log({a+b}{ а плюс ^бэ^ })",
+                    ["12"], 'algorithms'),
+    create_question("На каком языке пишут приложения для IOS?",
+                    ["swift", "свифт", "обжектив-си", "objective-c"], 'mobile'),
+    create_question("Как называется ^самый^ распространённый язык для работы с реляционными базами данных?",
+                    ["sql"], 'backend'),
+    create_question("Как называется вид инъекции, направленный на взлом реляционных баз данных?",
+                    ["sql", "sql-injection", "sql-инъекция"], 'security'),
+    create_question("Интерфейс для взаимодействия других программ с вашим сервисом",
+                    ["api", "апи"], 'backend'),
+    create_question("Заполните многоточие, чтобы выдать всем пользователям все права для этого файла: \nsudo chmod " +
+                    "<...> /vezdekod/vezdehits.txt?",
+                    ["777"], 'security'),
+    create_question("Какую сред`у разработки используют для создания android-приложений?",
+                    ["android studio, androidstudio"], 'mobile'),
+    create_question("Как`ой `алгоритм имеет в ^худшем^ случае асимпт`отитическую сложность {nlogn}{эн лог эн}?" +
+                    "\n1. quickSort(X)\n2. bubbleSort(X)\n3. mergeSort(X)",
+                    ["3"], 'algorithms')
 ]
 
 

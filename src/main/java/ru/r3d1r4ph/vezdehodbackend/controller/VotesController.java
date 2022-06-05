99b794aa92e4e07d0b80767aef84279f071802d4
LAPTOP-465MA33N\Армен <rediraph@mail.ru>
2022-06-05 17:19:11 +0700

    task 20 done

--- src/main/java/ru/r3d1r4ph/vezdehodbackend/controller/VotesController.java
+++ src/main/java/ru/r3d1r4ph/vezdehodbackend/controller/VotesController.java
@@ -1,16 +1,21 @@
 package ru.r3d1r4ph.vezdehodbackend.controller;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.r3d1r4ph.vezdehodbackend.data.Participants;
-import ru.r3d1r4ph.vezdehodbackend.dto.MessageDto;
 import ru.r3d1r4ph.vezdehodbackend.dto.ParticipantDto;
 import ru.r3d1r4ph.vezdehodbackend.dto.VoteDto;
 import ru.r3d1r4ph.vezdehodbackend.service.VotesService;
 
+import java.time.Duration;
 import java.util.List;
 
 @RestController
@@ -20,31 +25,66 @@ public class VotesController {
 
     private final VotesService votesService;
 
+    private Bucket bucket;
+
+    @Value("${bucket_limit:100}")
+    private Integer BucketLimit;
+
+    @Value("${fill_rate:100}")
+    private Integer FillRate;
+
+    public void initBucket() {
+        if (this.bucket == null) {
+            Bandwidth limit = Bandwidth.classic(BucketLimit, Refill.greedy(FillRate, Duration.ofMinutes(1)));
+            this.bucket = Bucket4j.builder()
+                    .addLimit(limit)
+                    .build();
+        }
+    }
+
     @PostMapping
-    public ResponseEntity<MessageDto> vote(@Validated @RequestBody VoteDto voteDto) {
-        if (Participants.ParticipantsMap.containsKey(voteDto.getArtist())) {
-            Participants.ParticipantsMap.put(
-                    voteDto.getArtist(),
-                    Participants.ParticipantsMap.get(voteDto.getArtist()) + 1
-            );
-            return ResponseEntity
-                    .status(HttpStatus.CREATED)
-                    .body(new MessageDto(
-                            "Created"
-                    ));
+    public ResponseEntity<Void> vote(@Validated @RequestBody VoteDto voteDto) {
+        initBucket();
+        var consumptionProbe = bucket.tryConsumeAndReturnRemaining(1);
+        if (consumptionProbe.isConsumed()) {
+            if (Participants.ParticipantsMap.containsKey(voteDto.getArtist())) {
+                Participants.ParticipantsMap.put(
+                        voteDto.getArtist(),
+                        Participants.ParticipantsMap.get(voteDto.getArtist()) + 1
+                );
+                return ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .build();
+            } else {
+                return ResponseEntity
+                        .status(HttpStatus.NOT_FOUND)
+                        .build();
+            }
         } else {
             return ResponseEntity
-                    .status(HttpStatus.NOT_FOUND)
-                    .body(new MessageDto(
-                            "NotFound"
-                    ));
+                    .status(HttpStatus.TOO_MANY_REQUESTS)
+                    .header("x-ratelimit-limit", BucketLimit.toString())
+                    .header("x-ratelimit-remaining", String.valueOf(consumptionProbe.getRemainingTokens()))
+                    .header("x-ratelimit-reset", String.valueOf(consumptionProbe.getNanosToWaitForRefill()))
+                    .build();
         }
     }
 
     @GetMapping
     public ResponseEntity<List<ParticipantDto>> getVotes() {
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(votesService.getParticipantsList());
+        initBucket();
+        var consumptionProbe = bucket.tryConsumeAndReturnRemaining(1);
+        if (consumptionProbe.isConsumed()) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(votesService.getParticipantsList());
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.TOO_MANY_REQUESTS)
+                    .header("x-ratelimit-limit", BucketLimit.toString())
+                    .header("x-ratelimit-remaining", String.valueOf(consumptionProbe.getRemainingTokens()))
+                    .header("x-ratelimit-reset", String.valueOf(consumptionProbe.getNanosToWaitForRefill()))
+                    .build();
+        }
     }
 }
--- src/main/java/ru/r3d1r4ph/vezdehodbackend/service/VotesService.java
+++ src/main/java/ru/r3d1r4ph/vezdehodbackend/service/VotesService.java
@@ -1,5 +1,6 @@
 package ru.r3d1r4ph.vezdehodbackend.service;
 
+import org.springframework.stereotype.Service;
 import ru.r3d1r4ph.vezdehodbackend.dto.ParticipantDto;
 
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.stream.Collectors;
 
 import static ru.r3d1r4ph.vezdehodbackend.data.Participants.ParticipantsMap;
 
+@Service
 public class VotesService {
 
     public List<ParticipantDto> getParticipantsList() {
